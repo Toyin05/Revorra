8b1f0066062f282096337ce766028a533ab5d924
Toyin <oluwatoyinojumoro@gmail.com>
2026-05-03 16:40:36 +0100

    removed ads

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
 import { DashboardLayout } from "@/components/DashboardLayout";
 import ProtectedRoute from "@/components/ProtectedRoute";
-import AdLoader from "@/components/AdLoader";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -38,7 +37,6 @@ const App = () => (
       <Sonner />
       <AuthProvider>
         <BrowserRouter>
-          <AdLoader />
           <Routes>
             {/* Public */}
             <Route path="/" element={<Home />} />
